cc979f3fcd1b018e6d44f37b5c92993a735aaa3b
21IST2Flerov <99124529+21IST2Flerov@users.noreply.github.com>
2022-02-23 01:44:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,83 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  int k = sqrt(value);
+  bool checking = 0;
+  int counting  = 0;
+  for (int i = 0; i <=k; i++) {
+    if ( 0 == value%i) {
+      counting += 1;
+    }
+  }
+  if (0 == counting){
+    checking = true;
+    return checking;
+  } else {
+    return checking;
+  }
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  int flag = 1;
+    int num = 0;
+    uint64_t cnt_Prime = 0;
+    for (int i = 2; 0 != flag; i++) {
+        num = i;
+        int counting = 0;
+        int k = sqrt(num);
+        for (int i = 2; i <= k; i++) {
+            if (0 == num%i) {
+                counting += 1;
+                break;
+            }
+        }
+        if (0 == counting) {
+            cnt_Prime += 1;
+            if (cnt_Prime == n) {
+                flag = 0;
+            }
+        }
+    }
+    return num;
+}
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  int flag = 1;
+    uint64_t num = 0;
+    for (uint64_t i = value+1; 0 != flag; i++) {
+        num = i;
+        int counting = 0;
+        int k = sqrt(num);
+        for (int i = 2; i <= k; i++) {
+            if (0 == num%i) {
+                counting += 1;
+                break;
+            }
+        }
+        if (0 == counting) {
+            flag = 0;
+        }
+    }
+    return num;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  uint64_t num = 0;
+    uint64_t sum = 0;
+    for (uint64_t i = 2; i < hbound; i++) {
+        num = i;
+        int counting = 0;
+        int k = sqrt(num);
+        for (int i = 2; i <= k; i++) {
+            if (0 == num%i) {
+                counting += 1;
+                break;
+            }
+        }
+        if (0 == counting) {
+            sum += num;
+        }
+    }
+    return sum;
 }
